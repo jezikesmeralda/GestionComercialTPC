f27c6315ec155762aea3f86398da25e13c5bd636
TheoMartinezYoma <theovalentinmartinez01@gmail.com>
2026-06-19 15:01:14 -0300

    Add stored procedure sp_ListarProductos
    Added a stored procedure to list product details with joins.

--- Scripts/Cambios.sql
+++ Scripts/Cambios.sql
@@ -6,4 +6,29 @@ GO
 
 
 SELECT * FROM Productos;
+--AÑADI ESTO---
+ALTER PROCEDURE sp_ListarProductos
+AS
+BEGIN
+    SELECT
+        p.Id,
+        p.NombreProducto,
+        p.Descripcion,
+        p.ImagenUrl,
+        p.PrecioCosto,
+        p.PorcentajeGanancia,
+        p.PrecioVenta,
+        p.StockActual,
+        p.StockMinimo,
+        p.Activo,
 
+        m.Id AS IdMarca,
+        m.Nombre AS NombreMarca,
+
+        c.Id AS IdCategoria,
+        c.Nombre AS NombreCategoria
+
+    FROM Productos p
+    INNER JOIN Marcas m ON p.IdMarca = m.Id
+    INNER JOIN Categorias c ON p.IdCategoria = c.Id
+END
