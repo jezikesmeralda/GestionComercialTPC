d39d29f769184d2f381402f3f49b8a94ff3a329a
TheoMartinezYoma <theovalentinmartinez01@gmail.com>
2026-07-11 13:17:27 -0300

    Add stored procedures

--- Scripts/Cambios.sql
+++ Scripts/Cambios.sql
@@ -12,6 +12,100 @@ SELECT * FROM DetalleCompras;
 
 GO
 --AÑADI ESTO---
+ALTER PROCEDURE sp_AltaVenta
+    @IdCliente INT,
+    @IdUsuario INT,
+    @Total DECIMAL(18,2),
+    @MedioPago VARCHAR(20),
+    @Cuotas INT,
+    @Interes DECIMAL(5,2),
+    @TotalConInteres DECIMAL(18,2)
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    DECLARE @NumeroFactura VARCHAR(20);
+
+    SET @NumeroFactura = 
+        'FAC-' + RIGHT('000000' + CAST((SELECT ISNULL(MAX(Id),0)+1 FROM Ventas) AS VARCHAR), 6);
+
+
+    INSERT INTO Ventas
+    (
+        IdCliente,
+        IdUsuario,
+        FechaVenta,
+        NumeroFactura,
+        Total,
+        MedioPago,
+        Cuotas,
+        Interes,
+        TotalConInteres
+    )
+    VALUES
+    (
+        @IdCliente,
+        @IdUsuario,
+        GETDATE(),
+        @NumeroFactura,
+        @Total,
+        @MedioPago,
+        @Cuotas,
+        @Interes,
+        @TotalConInteres
+    );
+
+
+    DECLARE @IdVenta INT = SCOPE_IDENTITY();
+
+
+    SELECT 
+        Id,
+        NumeroFactura
+    FROM Ventas
+    WHERE Id = @IdVenta;
+END
+
+GO
+
+ALTER PROCEDURE [dbo].[sp_ObtenerVentaPorId]
+    @IdVenta INT
+AS
+BEGIN
+    SET NOCOUNT ON;
+    SELECT
+        v.Id,
+        v.NumeroFactura,
+        v.FechaVenta,
+        v.Total,
+        v.MedioPago,
+        v.Cuotas,
+        v.Interes,
+        v.TotalConInteres,
+        c.Id AS IdCliente,
+        c.Nombre AS NombreCliente,
+        c.Apellido AS ApellidoCliente,
+        c.Email AS EmailCliente,
+        u.Id AS IdUsuario,
+        u.Nombre AS NombreUsuario
+    FROM Ventas v
+    INNER JOIN Clientes c ON v.IdCliente = c.Id
+    INNER JOIN Usuarios u ON v.IdUsuario = u.Id
+    WHERE v.Id = @IdVenta;
+
+    SELECT
+        dv.Id,
+        dv.IdProducto,
+        p.NombreProducto,
+        dv.Cantidad,
+        dv.PrecioUnitario,
+        dv.Subtotal
+    FROM DetalleVentas dv
+    INNER JOIN Productos p ON dv.IdProducto = p.Id
+    WHERE dv.IdVenta = @IdVenta;
+END
+
+GO
 ALTER PROCEDURE sp_ListarProductos
 AS
 BEGIN
